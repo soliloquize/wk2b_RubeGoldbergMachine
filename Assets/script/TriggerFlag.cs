a39c506331caa51ad6e5b729846a37d78ed7c20e
Jun Ma <seven2277s@gmail.com>
2015-07-21 11:27:13 -0400

    2nd commit, add particle and background music

---
+++ Assets/script/TriggerFlag.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerFlag : MonoBehaviour {
+
+	public GameObject flag;
+	public ParticleSystem fireWork1;
+	public ParticleSystem fireWork2;
+
+	void OnTriggerEnter(){
+		flag.GetComponent<RaiseFlag>().DoRaiseFlag();
+		StartCoroutine(FireWork());
+	}
+
+	IEnumerator FireWork(){
+
+		yield return new WaitForSeconds(1.5f);
+		fireWork1.Play();
+		fireWork2.Play();
+	}
+
+}
